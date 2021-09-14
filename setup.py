bd3a58171238e6e40bb7f70b7b1c81ce79c23252
Ryuichi Yamamoto <zryuichi@gmail.com>
2021-09-14 13:42:53 +0900

    Fix #20, check return code for cmake

--- setup.py
+++ setup.py
@@ -147,7 +147,8 @@ if not exists(join(src_top, "mecab", "src", "config.h")):
     build_dir = join(src_top, "build")
     os.makedirs(build_dir, exist_ok=True)
     os.chdir(build_dir)
-    run(["cmake", ".."])
+    r = run(["make", ".."])
+    r.check_returncode()
     os.chdir(cwd)
 
 all_src = []
