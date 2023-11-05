6928f45deb17b857e4535559f293077a9d581a94
Ryuichi Yamamoto <zryuichi@gmail.com>
2023-11-05 11:32:48 +0900

    start new dev cycle

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import Extension, find_packages, setup
 
 platform_is_windows = sys.platform == "win32"
 
-version = "0.3.3"
+version = "0.3.4-dev"
 
 min_cython_ver = "0.21.0"
 try:
