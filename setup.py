a3e2115c52bbc71bc816fdf7ad29e11300b1db35
Ryuichi Yamamoto <zryuichi@gmail.com>
2024-07-13 16:04:09 +0900

    bump new dev ver

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import Extension, find_packages, setup
 
 platform_is_windows = sys.platform == "win32"
 
-version = "0.3.4"
+version = "0.3.5-dev"
 
 msvc_extra_compile_args_config = [
     "/source-charset:utf-8",
