c362daec7ddcd55960029af45874810fce9666fc
sabonerune <102559104+sabonerune@users.noreply.github.com>
2024-10-21 18:21:05 +0900

    BLD: Declarative config
    Move most of the configuration from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -4,17 +4,12 @@ import sys
 from glob import glob
 from itertools import chain
 from os.path import exists, join
-from subprocess import run
 
 import numpy as np
 import setuptools.command.build_ext
-import setuptools.command.build_py
-import setuptools.command.develop
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 
 platform_is_windows = sys.platform == "win32"
-
-version = "0.3.5-dev"
 
 msvc_extra_compile_args_config = [
     "/source-charset:utf-8",
@@ -93,7 +88,9 @@ if not exists(join(src_top, "mecab", "src", "config.h")):
 
     # NOTE: The wrapped OpenJTalk does not depend on HTS_Engine,
     # but since HTSEngine is included in CMake's dependencies, it refers to a dummy path.
-    r = run(["cmake", "..", "-DHTS_ENGINE_INCLUDE_DIR=.", "-DHTS_ENGINE_LIB=dummy"])
+    r = subprocess.run(
+        ["cmake", "..", "-DHTS_ENGINE_INCLUDE_DIR=.", "-DHTS_ENGINE_LIB=dummy"]
+    )
     r.check_returncode()
     os.chdir(cwd)
 
@@ -155,109 +152,4 @@ ext_modules += [
     )
 ]
 
-# Adapted from https://github.com/pytorch/pytorch
-cwd = os.path.dirname(os.path.abspath(__file__))
-if os.getenv("PYOPENJTALK_BUILD_VERSION"):
-    version = os.getenv("PYOPENJTALK_BUILD_VERSION")
-else:
-    try:
-        sha = (
-            subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=cwd)
-            .decode("ascii")
-            .strip()
-        )
-        version += "+" + sha[:7]
-    except subprocess.CalledProcessError:
-        pass
-    except IOError:  # FileNotFoundError for python 3
-        pass
-
-
-class build_py(setuptools.command.build_py.build_py):
-    def run(self):
-        self.create_version_file()
-        setuptools.command.build_py.build_py.run(self)
-
-    @staticmethod
-    def create_version_file():
-        global version, cwd
-        print("-- Building version " + version)
-        version_path = os.path.join(cwd, "pyopenjtalk", "version.py")
-        with open(version_path, "w") as f:
-            f.write("__version__ = '{}'\n".format(version))
-
-
-class develop(setuptools.command.develop.develop):
-    def run(self):
-        build_py.create_version_file()
-        setuptools.command.develop.develop.run(self)
-
-
-with open("README.md", "r", encoding="utf8") as fd:
-    long_description = fd.read()
-
-setup(
-    name="pyopenjtalk",
-    version=version,
-    description="A python wrapper for OpenJTalk",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Ryuichi Yamamoto",
-    author_email="zryuichi@gmail.com",
-    url="https://github.com/r9y9/pyopenjtalk",
-    license="MIT",
-    packages=find_packages(),
-    package_data={"": ["htsvoice/*"]},
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": custom_build_ext, "build_py": build_py, "develop": develop},
-    install_requires=[
-        "importlib_resources; python_version<'3.9'",
-        "numpy >= 1.20.0",
-        "tqdm",
-    ],
-    tests_require=["nose", "coverage"],
-    extras_require={
-        "docs": [
-            "sphinx_rtd_theme",
-            "nbsphinx>=0.8.6",
-            "Jinja2>=3.0.1",
-            "pandoc",
-            "ipython",
-            "jupyter",
-        ],
-        "dev": [
-            "cython>=0.28.0",
-            "pysen",
-            "types-setuptools",
-            "mypy<=0.910",
-            "black>=19.19b0,<=20.8",
-            "click<8.1.0",
-            "flake8>=3.7,<4",
-            "flake8-bugbear",
-            "isort>=4.3,<5.2.0",
-            "types-decorator",
-            "importlib-metadata<5.0",
-        ],
-        "test": ["pytest", "scipy"],
-        "marine": ["marine>=0.0.5"],
-    },
-    classifiers=[
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Cython",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Software Development",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-    ],
-    keywords=["OpenJTalk", "Research"],
-)
+setup(ext_modules=ext_modules, cmdclass={"build_ext": custom_build_ext})
